c4744a1a0ab085881e6dfde3b96d2080d73b25b0
cgrogers9 <c.rogers@standarduserllc.com>
2022-12-01 11:41:39 -0600

    password

--- Group/Assignment 1/DiceWare.py
+++ Group/Assignment 1/DiceWare.py
@@ -1,19 +1,21 @@
 import time
-import datetime
-import json
-
 
 #roll dice with time function 
 def roll_dice():
-    #we need to make this more random 
-    curr = str(time.time())
+ 
+    curr = str(time.time()).split('.') #getting current time and splitting it into two strings after the decimal place
 
-    dice = []
+    curr = curr[1] #taking the second string (numbers after the decimal point)
+
+    dice = [] #empty list to store the dice roll 
 
     while True:
-        for cu in curr:
+        #for loop to filter through each number in the string 
+        for cu in curr: 
             try:
-                num = int(cu)
+                num = int(cu) #turn each element into integer data type 
+
+                #get only the number between 1 and 6 and collect only 5 numbers for the roll 
                 if num > 0  and num < 7:
                     dice.append(cu)
                     if len(dice) == 5:
@@ -29,29 +31,40 @@ def roll_dice():
 
 
 #generate words from dice roll (pass in roll variable)
-def get_words(roll):
+def get_word(roll):
 
+    #open text file 
     file = open("words.txt")
     data = file.readlines()
-    matchwords = []
+    matchwords = [] #empty list for the matchwords 
     for line in data:
         row = line.split()
         if row[0] == roll:
             matchwords.append(row[1])
 
-    return matchwords
+    return matchwords #return word that matches with roll 
 
 
-def pass_phrase():
+#calulate number of possible passwords based on number of rolls 
+import math
+def possible_passwords(num_rolls):
     
+    #initialize the number of items to chose from 
+    n = 7776
+
+    #initialize possiblilies to choose from 
+    k = num_rolls
     
+    passwords = math.comb(n,k)
 
-
+    #return total number of combinations formatted 
+    return format(passwords, ',d')
 
 #main function
 def main():
-    
+    t = 5
     #return
 
-user = roll_dice()
+
+user = possible_passwords(2)
 print(user)
---
+++ testing.py
@@ -0,0 +1,5 @@
+import time
+
+curr_time = str(round(time.time()*100000))
+
+print(curr_time)
